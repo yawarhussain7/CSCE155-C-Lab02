c2bee780ab8e01ca3a9a67322c764d0c8b4ab82e
Yawar Hussain <yawarhussain793@gmail.com>
2023-10-18 20:46:28 +0530

    Create a c file with name Dollar.c and add program introduction and basic C sytax

---
+++ Dollar.c
@@ -0,0 +1,12 @@
+/*  
+    Author: Yawar Hussain
+    This program get USD from user and exchange in Japapnese Yen and British Pound 
+    with 10 % of exchange rate 
+*/
+#include<stdio.h>
+
+int main()
+{
+
+    return 0;
+}
