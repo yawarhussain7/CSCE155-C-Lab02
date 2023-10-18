3a7361562b5138becc586e181b5ce9799188ade5
Yawar Hussain <yawarhussain793@gmail.com>
2023-10-18 20:54:35 +0530

    Create a variable in Dollar.c file

--- Dollar.c
+++ Dollar.c
@@ -3,13 +3,21 @@
     This program get USD from user and exchange in Japapnese Yen and British Pound 
     with 10 % of exchange rate 
 
-    1 USD = 127.65 Yen
+    1 USD = 127.65 YEN
     1 USD = 0.79 GBP
 */
 #include<stdio.h>
 
 int main()
 {
+    const float GBP = 0.79;
+    const float YEN = 127.65;
+
+    double USD, JPY,Pound,excange_rate ;
+
+
+
+
 
     return 0;
 }
