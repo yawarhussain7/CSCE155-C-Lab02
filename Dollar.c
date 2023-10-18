22c7ab387652a46160e328a44ccbd7932379a863
Yawar Hussain <yawarhussain793@gmail.com>
2023-10-18 20:47:46 +0530

    Add given statement things

--- Dollar.c
+++ Dollar.c
@@ -2,6 +2,9 @@
     Author: Yawar Hussain
     This program get USD from user and exchange in Japapnese Yen and British Pound 
     with 10 % of exchange rate 
+
+    1 USD = 127.65 Yen
+    1 USD = 0.79 GBP
 */
 #include<stdio.h>
 
